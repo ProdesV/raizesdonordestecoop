bbc059c48906ab7bf4c313e1db166b5de1da5807
João Vitor <oliveirasouzajoaovitor68@gmail.com>
2025-09-11 11:14:01 -0300

    Update ex.js

--- ex.js
+++ ex.js
@@ -3,6 +3,12 @@ const toggle = document.querySelector('.menu-toggle');
 const menu = document.querySelector('.menu');
 
 toggle.addEventListener('click', () => {
+  console.log('Botão clicado!');
+  menu.classList.toggle('active');
+});
+
+
+toggle.addEventListener('click', () => {
   menu.classList.toggle('active');
 });
 
